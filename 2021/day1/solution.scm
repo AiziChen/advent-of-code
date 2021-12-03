338d991d8b875fdef2a52dc4910462a2532ad248
AiziChen <1098679804@qq.com>
2021-12-03 18:51:41 +0800

    add part2 to day1

--- 2021/day1/solution.scm
+++ 2021/day1/solution.scm
@@ -8,15 +8,28 @@
            [else
             (cons n (loop (get-line p)))]))))))
 
-(define calc
+(define part1
   (lambda (op ms)
     (cond
      [(null? (cdr ms)) 0]
      [(op (car ms) (cadr ms))
-      (+ 1 (calc op (cdr ms)))]
+      (+ 1 (part1 op (cdr ms)))]
      [else
-      (calc op (cdr ms))])))
+      (part1 op (cdr ms))])))
 
-;;; How many measurements are larger than the previous measurement?
+(define part2
+  (lambda (op ms)
+    (cond
+     [(= (length ms) 3) 0]
+     [(op (+ (car ms) (cadr ms) (list-ref ms 2))
+         (+ (cadr ms) (list-ref ms 2) (list-ref ms 3)))
+      (+ 1 (part2 op (cdr ms)))]
+     [else
+      (part2 op (cdr ms))])))
+
+
 (let ([measurements (map string->number (file->lines "input"))])
-  (printf "answer: ~a~n" (calc < measurements)))
+  ;;; How many measurements are larger than the previous measurement?
+  (printf "part1 answer: ~a~n" (part1 < measurements))
+  ;; How many sums are larger than the previus sum?
+  (printf "part2 answer: ~a~n" (part2 < measurements)))
